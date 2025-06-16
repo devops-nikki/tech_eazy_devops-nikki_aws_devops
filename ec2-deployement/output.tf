26a9aef13ed5f18eaf87ae0f33f6a979cf7cdb91
nikki <devops.nikki@gmail.com>
2025-06-16 12:01:25 +0000

    Update README.md

--- ec2-deployement/output.tf
+++ ec2-deployement/output.tf
@@ -1,4 +1,4 @@
-output "instance_public_ip" {
+output "app_ec2_public_ip" {
+  description = "Public IP of the EC2 instance running the Java app"
   value       = aws_instance.app_server.public_ip
-  description = "Public IP of the deployed EC2 instance"
 }
